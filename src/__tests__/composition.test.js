bb46758c4a0b2aaec2812fb610b77ffa7584017b
AlexMin314 <m98stone@gmail.com>
2018-11-21 17:41:46 +0800

    async pipe

--- src/__tests__/composition.test.js
+++ src/__tests__/composition.test.js
@@ -1,5 +1,6 @@
 import {
   pipe,
+  pipeA,
   compose
 } from '../composition';
 
@@ -14,6 +15,24 @@ describe('composition functions', () => {
       )(1)).toBe('6');
     });
   });
+  describe('pipeA', () => {
+    test('should return left to right composed async/await(or promise) function', async () => {
+      const result = await pipeA(
+        async (n) => await n * 2,
+        async (n) => {
+          const result = await new Promise(resolve => setTimeout(() => {
+            return resolve(n * 2);
+          }, 200));
+          return result;
+        },
+        (n) => new Promise(resolve => setTimeout(() => {
+          return resolve(n * 2);
+        }, 200)),
+      )(1);
+      expect(result).toBe(8);
+    });
+  });
+
   describe('compose', () => {
     test('should return right to left composed function', () => {
       expect(compose(
--- src/composition.js
+++ src/composition.js
@@ -8,6 +8,9 @@ export const pipe = (...fns) => fns.reduce((prevFn, nextFn) => {
   return (...args) => B(nextFn)(prevFn)(...args);
 }, identity);
 
+export const pipeA = (...fns) => (...args) =>
+  fns.reduce((chain, func) => chain.then(func), Promise.resolve(...args));
+
 // right to left
 export const compose = (...fns) => fns.reduceRight((lastFn, prevFn) => {
   return (...args) => B(prevFn)(lastFn)(...args);
