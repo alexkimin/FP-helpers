4fc8e74150a913fd65bb7f09b5f5bd2d5cb15df0
AlexMin314 <m98stone@gmail.com>
2018-11-21 14:44:05 +0800

    application

---
+++ src/__tests__/application.test.js
@@ -0,0 +1,19 @@
+import {
+  apply,
+  call,
+} from '../application';
+
+describe('application functions', () => {
+
+  describe('apply', () => {
+    test('should return applied value', () => {
+      expect(apply(Math.max, [1, 2])).toBe(2);
+    });
+  });
+  describe('call', () => {
+    test('should return applied value', () => {
+      expect(call(Math.max, 1, 2)).toBe(2);
+    });
+  });
+
+});
