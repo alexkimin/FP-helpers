b8b787d05a54c141bb39661acc12ccf109f23395
AlexMin314 <m98stone@gmail.com>
2018-11-22 22:51:08 +0800

    composition

--- src/__tests__/composition.test.js
+++ src/__tests__/composition.test.js
@@ -1,63 +1,71 @@
 import {
   pipe,
   pipeA,
+  pipeP,
   compose,
-  composeA
+  composeA,
+  composeP,
 } from '../composition';
 
 describe('composition functions', () => {
 
+  const fns1 = [
+    n => n * 2,
+    n => n * 3,
+    n => n.toString(),
+  ];
+
+  const fns2 = [
+    async (n) => await n * 2,
+    async (n) => {
+      const result = await new Promise(resolve => setTimeout(() => {
+        return resolve(n * 2);
+      }, 200));
+      return result;
+    },
+    (n) => new Promise(resolve => setTimeout(() => {
+      return resolve(n * 2);
+    }, 200)),
+  ];
+
   describe('pipe', () => {
     test('should return left to right composed function', () => {
-      expect(pipe(
-        n => n * 2,
-        n => n * 3,
-        n => n.toString(),
-      )(1)).toBe('6');
+      expect(pipe(...fns1)(1)).toBe('6');
     });
   });
   describe('pipeA', () => {
     test('should return left to right composed async/await(or promise) function', async () => {
-      const result = await pipeA(
-        async (n) => await n * 2,
-        async (n) => {
-          const result = await new Promise(resolve => setTimeout(() => {
-            return resolve(n * 2);
-          }, 200));
-          return result;
-        },
-        (n) => new Promise(resolve => setTimeout(() => {
-          return resolve(n * 2);
-        }, 200)),
-      )(1);
+      const result = await pipeA(...fns2)(1);
+      expect(result).toBe(8);
+    });
+  });
+  describe('pipeP', () => {
+    test('should work same as pipeA', async () => {
+      const result = await pipeP(...fns2)(1);
       expect(result).toBe(8);
     });
   });
 
   describe('compose', () => {
     test('should return right to left composed function', () => {
-      expect(compose(
-        n => n * 2,
-        n => n * 3,
-        n => n.toString(),
-      )(1)).toBe(6);
+      expect(compose(...fns1)(1)).toBe(6);
     });
   });
   describe('composeA', () => {
     test('should return right to left composed async/await(or promise) function', async () => {
-      const result = await composeA(
+      const result = await composeA(...[
         n => n.toString(),
-        async (n) => await n * 2,
-        async (n) => {
-          const result = await new Promise(resolve => setTimeout(() => {
-            return resolve(n * 2);
-          }, 200));
-          return result;
-        },
-        (n) => new Promise(resolve => setTimeout(() => {
-          return resolve(n * 2);
-        }, 200)),
-      )(1);
+        ...fns2,
+      ])(1);
+      expect(result).toBe('8');
+    });
+  });
+  describe('composeP', () => {
+    test('should work same as composeA', async () => {
+      const result = await composeP(...[
+        n => n.toString(),
+        ...fns2,
+      ])(1);
       expect(result).toBe('8');
     });
   });
--- src/composition.js
+++ src/composition.js
@@ -10,13 +10,20 @@ export const pipe = (...fns) => fns.reduce((prevFn, nextFn) => {
   return (...args) => B(nextFn)(prevFn)(...args);
 }, identity);
 
-export const pipeA = (...fns) => (...args) =>
+export const pipeP = (...fns) => (...args) =>
   fns.reduce((chain, func) => chain.then(func), Promise.resolve(...args));
+
+export const pipeA = (...fns) => (...args) =>
+  fns.reduce(async (prevFn, nextFn) => await nextFn(await prevFn), args);
 
 // right to left
 export const compose = (...fns) => fns.reduceRight((lastFn, prevFn) => {
   return (...args) => C(lastFn)(prevFn)(...args);
 }, identity);
 
-export const composeA = (...fns) => (...args) =>
+export const composeP = (...fns) => (...args) =>
   fns.reduceRight((chain, func) => chain.then(func), Promise.resolve(...args));
+
+export const composeA = (...fns) => (...args) =>
+  fns.reduceRight(async (prevFn, nextFn) => await nextFn(await prevFn), args);
+
