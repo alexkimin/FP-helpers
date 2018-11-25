8503d32c8c590d7b3b948767fb4f042f0f6cc530
AlexMin314 <m98stone@gmail.com>
2018-11-25 18:17:23 +0800

    validation

--- src/__tests__/validation.test.js
+++ src/__tests__/validation.test.js
@@ -1,5 +1,7 @@
 import {
   isFunction,
+  isIterable,
+  isPromise,
 } from '../validation';
 
 describe('valdation functions', () => {
@@ -11,6 +13,31 @@ describe('valdation functions', () => {
     });
     test('should return false if any arg is not a function type', () => {
       expect(isFunction(() => {}, () => {}, 'hello', () => {})).toBe(false);
+    });
+  });
+
+  describe('isIterable', () => {
+    test('should return false', () => {
+      expect(isIterable({})).toBe(false);
+      expect(isIterable(undefined)).toBe(false);
+      expect(isIterable(null)).toBe(false);
+      expect(isIterable(12)).toBe(false);
+    });
+    test('should return true', () => {
+      expect(isIterable([])).toBe(true);
+      expect(isIterable(new Map())).toBe(true);
+      expect(isIterable(new Set())).toBe(true);
+      expect(isIterable('hello')).toBe(true);
+    });
+  });
+
+  describe('isPromise', () => {
+    test('should return true', () => {
+      const test = () => new Promise(resolve => setTimeout(() => resolve(), 2000));
+      expect(isPromise(test())).toBe(true);
+    });
+    test('should return false', () => {
+      expect(isPromise({})).toBe(false);
     });
   });
 
--- src/validation.js
+++ src/validation.js
@@ -1,5 +1,9 @@
 // types
 export const isFunction = (...arg) => arg.every(fn => typeof fn === 'function');
 
-// protocol
 export const isIterable = arg => Symbol.iterator in Object(arg);
+
+export const isPromise = (promise) => {
+  if(!promise) return false;
+  return !!(promise.then && promise.catch);
+};
