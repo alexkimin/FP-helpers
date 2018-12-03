1861eda6c61e58616da9173ec39a7c83553c6e48
AlexMin314 <m98stone@gmail.com>
2018-12-03 14:23:11 +0800

    call

--- src/application.js
+++ src/application.js
@@ -3,4 +3,4 @@ import { curry2 } from './curry';
 // base
 export const apply = curry2((fn, arg) => fn(...arg));
 
-export const call = fn => (...args) => fn(...args);
+export const call = curry2((fn, ...args) => fn(...args));
