a97a24c50d445902b11f3fa17ba621cbbff916ad
AlexMin314 <m98stone@gmail.com>
2018-12-03 21:22:56 +0800

    refactor iter

--- src/__tests__/iter.test.js
+++ src/__tests__/iter.test.js
@@ -1,25 +1,31 @@
-import {
-  iterValues,
-  iterEntries,
-} from '../iter';
+import { Iter } from '../iter';
 import { isIterable } from '../validation';
 
 describe('iter functions', () => {
-  describe('iterValues', () => {
+  describe('Iter.values', () => {
     test('should work as expected', () => {
-      const iter = iterValues({ name: 'alex', age: 7 });
+      const iter = Iter.values({ name: 'alex', age: 7 });
       expect(isIterable(iter)).toBe(true);
       expect(iter.next()).toEqual({ value: 'alex', done: false });
       expect(iter.next()).toEqual({ value: 7, done: false });
       expect(iter.next()).toEqual({ value: undefined, done: true });
     });
   });
-  describe('iterEntries', () => {
+  describe('Iter.entries', () => {
     test('should work as expected', () => {
-      const iter = iterEntries({ name: 'alex', age: 7 });
+      const iter = Iter.entries({ name: 'alex', age: 7 });
       expect(isIterable(iter)).toBe(true);
       expect(iter.next()).toEqual({ value: ['name', 'alex'], done: false });
       expect(iter.next()).toEqual({ value: ['age', 7], done: false });
+      expect(iter.next()).toEqual({ value: undefined, done: true });
+    });
+  });
+  describe('Iter.keys', () => {
+    test('should work as expected', () => {
+      const iter = Iter.keys({ name: 'alex', age: 7 });
+      expect(isIterable(iter)).toBe(true);
+      expect(iter.next()).toEqual({ value: 'name', done: false });
+      expect(iter.next()).toEqual({ value: 'age', done: false });
       expect(iter.next()).toEqual({ value: undefined, done: true });
     });
   });
--- src/iter.js
+++ src/iter.js
@@ -1,31 +1,29 @@
-import { curry2 } from './curry';
 import { isIterable, isObject } from './validation';
 
-export const genToIter = curry2((generator, coll, iter = generator(coll)) => ({
-  next: () => iter.next(),
-  // iterator[Symbol.iterator]() == iterator
-  [Symbol.iterator]() { return this },
-}));
-
-const _iter = {
-  ...genToIter,
-  values: genToIter(function* (coll) {
-    if (!coll) return;
-    for (const key in coll) yield coll[key];
-  }),
-  entries: genToIter(function *(coll) {
-    if (!coll) return;
-    for (const key in coll) yield [key, coll[key]];
-  })
+const _iter = {};
+_iter.values = function* (coll) {
+  if (!coll) return;
+  for (const key in coll) yield coll[key];
+};
+_iter.entries = function* (coll) {
+  if (!coll) return;
+  for (const key in coll) yield [key, coll[key]];
+};
+_iter.keys = function* (coll) {
+  if (!coll) return;
+  for (const key in coll) yield key;
 };
 
-const _getIterMethod = name => coll =>
+// iterator[Symbol.iterator]() == iterator
+const _handleCollAsIter = name => coll =>
   isIterable(coll)
     ? coll[typeof coll[name] === 'function' ? name : Symbol.iterator]()
     : isObject(coll)
       ? _iter[name](coll)
       : coll;
 
-export const iterValues = _getIterMethod('values');
-
-export const iterEntries = _getIterMethod('entries');
+export const Iter = {
+  values: _handleCollAsIter('values'),
+  entries: _handleCollAsIter('entries'),
+  keys: _handleCollAsIter('keys'),
+};
