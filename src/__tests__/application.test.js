7df3ea122589363fd9b686060488d45522a8183c
AlexMin314 <m98stone@gmail.com>
2018-12-05 18:05:22 +0800

    then added

--- src/__tests__/application.test.js
+++ src/__tests__/application.test.js
@@ -1,6 +1,7 @@
 import {
   apply,
   call,
+  then,
 } from '../application';
 
 describe('application functions', () => {
@@ -10,11 +11,19 @@ describe('application functions', () => {
       expect(apply(Math.max)([1, 2])).toBe(2);
     });
   });
+
   describe('call', () => {
     test('should return applied value', () => {
       expect(call(Math.max, 1, 2)).toBe(2);
       expect(call(Math.max)(1, 2)).toBe(2);
       expect(call(Math.max)(1, 2)).toBe(2);
+    });
+  });
+
+  describe('then', () => {
+    test('should resolve promise', () => {
+      expect(then(v => v * 2, new Promise(res => res(2)))).resolves.toBe(4);
+      expect(then(v => v * 2, then(v => v * 2, new Promise(res => res(2))))).resolves.toBe(8);
     });
   });
 });
